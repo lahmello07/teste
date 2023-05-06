d0d9b5dc85e7971f33a006ac9cac5696ab6d0774
laura <laura.sena@etec.sp.gov.br>
2023-05-06 11:52:48 -0300

    06/05/23

--- lib/telas/login.dart
+++ lib/telas/login.dart
@@ -5,42 +5,68 @@ class Telalogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextStyle style = TextStyle(fontFamily: ' Montsserrat', fontSize: 20.0);
     return Scaffold(
-      appBar: AppBar( title: Text("Tela 01"),),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        print( "Icon foi clicado!");
-      }, child: Icon(Icons.add_link),
-      ),
-      body: 
-      SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [ 
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'E-mail:',
-                  border: OutlineInputBorder()
-                ),
+        appBar: AppBar(
+          title: Text("Tela 01"),
+          backgroundColor: Color.fromARGB(255, 196, 0, 245),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Icon foi clicado!");
+          },
+          child: Icon(Icons.add_link),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        labelText: 'E-mail:',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32.0),
+                        )),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        labelText: 'Password:',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32.0),
+                        )),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10, left: 25, right: 25),
+                    height: 50,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 196, 0, 245),
+                        borderRadius: BorderRadius.circular(30.0)),
+
+                  ),
+                   TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                    padding: const EdgeInsets.all(16.0),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Gradient'),
+                ],
               ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password:',
-                  border: OutlineInputBorder()
-                ),
-              ),
-              
-              ],
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
